4a18370a882f46e40901940fafb548d452d0359d
xcia4899 <xcia48996531@gmail.com>
2026-07-14 15:55:44 +0800

    基本版面

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 import { initMenu } from "./utils/menu.js";
 import { loadLayout } from "./layout.js";
 import { scrollAnimation } from "./utils/scrollAnimation.js";
-import { loadSvgSprite } from "./ulite/svgSprite.js";
+import { loadSvgSprite } from "./utils/svgSprite.js";
 import { initHeader } from "./header.js";
 
-import { btnHoverAni } from "./ulite/btnHoverAni.js";
+import { btnHoverAni } from "./utils/btnHoverAni.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     await loadSvgSprite();
